23fef7c98086ccbcb2a8d632e024f7c1a88639b7
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-04-10 15:50:16 -0300

    Update TipoTexto.java

--- TiposDeVariaveis/TipoTexto.java
+++ TiposDeVariaveis/TipoTexto.java
@@ -1,17 +1,23 @@
-package com.company.TiposDeVariaveis;
-
 import java.util.Scanner;
 
-public class TipoTexto {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public class TipoTexto {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-        String nome = scanner.nextLine();
-        System.out.println(" Olá "+ nome + "!");
+            //String pode ser usado tanto palavra ou uma letra!
 
-        scanner.close();
+            System.out.print("Digite seu nome: ");
+            String nome = scanner.nextLine();
 
-        Character variavelChar = 'A' ;
+            System.out.println(" Olá " + nome + "!");
 
+            //Character só pode ser Usado uma Letra (também um simbolo ou um numero)
+
+            System.out.print("Digite uma letra: ");
+            Character letra = scanner.nextLine().charAt(0);
+
+            System.out.println("letra digitada: " + letra);
+
+
+        }
     }
-}
