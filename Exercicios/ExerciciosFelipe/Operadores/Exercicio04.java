e3dd38795c603c5038e2261c1aa7ff3f8613db6b
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-04-11 19:14:11 -0300

    Add files via upload

---
+++ Exercicios/ExerciciosFelipe/Operadores/Exercicio04.java
@@ -0,0 +1,32 @@
+package com.company.operadores;
+
+/*
+Crie um programa que receba a idade de uma pessoa e a quantidade de tempo que essa pessoa contribuiu com a previdência,
+depois verifique se ela pode aposentar ou não.
+Nas regras desse programa a pessoa vai precisar ter, pelo menos, 55 anos
+e ter contribuído com, pelo menos, 25 anos.
+Se ela passar nessas duas condições, então mostre pra ela uma mensagem dizendo que ela pode aposentar.
+ */
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nDigite sua idade: ");
+        int informeAIdade = scanner.nextInt();
+        System.out.print("\nQuantidade de tempo que contribuiu com previencia: ");
+        int tempoComPrevidencia = scanner.nextInt();
+
+        boolean idadeMinima = informeAIdade >= 55 ;
+        boolean tempoMinimoComPrevidencia = tempoComPrevidencia >= 25;
+
+       if(idadeMinima & tempoMinimoComPrevidencia){
+           System.out.println("\nDados informados indica que o Senhor ou a Senhora pode-se aposentar");
+       } else {
+           System.out.println("\nOs dados informados indica que o Senhor ou Senhora não pode-se aposentar");
+       }
+
+    }
+}
