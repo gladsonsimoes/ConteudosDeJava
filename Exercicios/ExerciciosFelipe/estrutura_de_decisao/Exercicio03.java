820328c91c4187612c530890395be47a6c299c6a
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-04-12 21:15:02 -0300

    Update Exercicio03.java

--- Exercicios/ExerciciosFelipe/estrutura_de_decisao/Exercicio03.java
+++ Exercicios/ExerciciosFelipe/estrutura_de_decisao/Exercicio03.java
@@ -3,6 +3,7 @@ package com.company.estrutura_de_decisao;
 System.out.print("Digite o número referente ao dia da semana: ");
 Imprima o nome do dia da semana (domingo, segunda-feira, etc.)
  */
+
 import java.util.Scanner;
 
 public class Exercicio03 {
@@ -11,30 +12,37 @@ public class Exercicio03 {
 
         System.out.print("\nDigite o numero referente ao dia da semana: ");
         int diaDaSemana = scanner.nextInt();
-        System.out.println("Dia da semana: \n");
+        String dia;
+
         switch (diaDaSemana) {
             case 1:
-                System.out.println("Domingo");
+                dia = "Domingo";
                 break;
-            case 2 :
-                System.out.println("Segunda-feira");
+            case 2:
+                dia = "Segunda-Feira";
                 break;
-            case 3 :
-                System.out.println("Terça-feira");
+            case 3:
+                dia = "Terça-Feira";
                 break;
-            case 4 :
-                System.out.println("Quarta-feira");
+            case 4:
+                dia = "Quarta-feira";
                 break;
-            case 5 :
-                System.out.println("Quinta-feira");
+            case 5:
+                dia = "Quinta-Feira";
                 break;
-            case 6 :
-                System.out.println("Sexta-feira");
+            case 6:
+                dia = "Sexta-Feira";
                 break;
-            case 7 :
-                System.out.println("Sabado");
+            case 7:
+                dia = "Sabado";
                 break;
+            default:
+                dia = "invalido";
         }
+        System.out.println("O dia da semana é: " + dia);
+        scanner.close();
+
 
     }
 }
+
