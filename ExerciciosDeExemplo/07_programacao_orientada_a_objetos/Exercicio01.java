d901ab76a06a0a2c0eee33fcb691c482bfd29387
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-04-27 15:19:29 -0300

    Update Exercicio01.java

--- ExerciciosDeExemplo/07_programacao_orientada_a_objetos/Exercicio01.java
+++ ExerciciosDeExemplo/07_programacao_orientada_a_objetos/Exercicio01.java
@@ -1,33 +1,47 @@
-ackage com.company.programacao_orientada_a_objetos;
+package com.company.programacao_orientada_a_objetos;
 
 /*
 Método que informa necessidade de repor estoque
 
-Crie um programa que contenha um método que verifique a necessidade de repor o estoque de determinado produto em uma
+Crie um programa que contenha um método que verifique a necessidade de repor o estoque de determinado
+produto em uma
 loja qualquer.
-Considere que a quantidade mínima de um produto deva ser 10. Se o estoque for menor que isso, então ele
+Considere que a quantidade mínima de um produto deva ser 10.
+Se o estoque for menor que isso, então ele
 retornará *true*, pois, será necessária a reposição do estoque, caso contrário, ele retornará false.
 
-Como parâmetro, esse método deverá receber um tipo que você irá criar e chamar de Produto, ou seja, você vai criar uma
-classe chamada Produto com os atributos necessários.
-
+//criar classe
+Como parâmetro, esse método deverá receber um tipo que você irá criar e chamar de Produto, ou seja,
+você vai criar uma
+classe chamada Produto com os atributos necessários.!!
 Os atributos da classe serão nome e quantidadeEstoque.
-
 Feito isso, implemente o método main do programa.
 
+//criar uma variavel
 Nele você vai criar uma variável do tipo Produto e depois usar essa
 variável para invocar o método que valida a necessidade de reposição de estoque.
 
 No final, você vai exibir, no console, se é necessária a reposição ou não.
+
  */
 
 public class Exercicio01 {
     public static void main(String[] args) {
         Produto1 produto = new Produto1();
 
-        produto.nome = "produtor";
-        produto.quantidadeEstoque = 10;
+        produto.nome = "Lojas Americanas";
+        produto.Produto = 10;
+        produto.quantidadeMinima = 10;
 
+        reposicaoEstoque(produto);
+    }
 
+    static void reposicaoEstoque(Produto1 produto) {
+        Boolean reposicao = produto.Produto < produto.quantidadeMinima;
+        if (reposicao) {
+            System.out.println("Precisa-se de Reposição");
+        } else {
+            System.out.println("Não precisa fazer a reposição");
+        }
     }
 }
