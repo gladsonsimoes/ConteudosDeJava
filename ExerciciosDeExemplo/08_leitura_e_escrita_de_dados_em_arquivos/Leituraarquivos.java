f907518e9c7e4bfd2bd27b8d84647dbc82fef5aa
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-04 19:14:03 -0300

    Update Leituraarquivos.java

--- ExerciciosDeExemplo/08_leitura_e_escrita_de_dados_em_arquivos/Leituraarquivos.java
+++ ExerciciosDeExemplo/08_leitura_e_escrita_de_dados_em_arquivos/Leituraarquivos.java
@@ -10,10 +10,16 @@ public class LeituraArquivos {
 
     public static void main(String[] args) throws IOException {
         Path arquivo = Paths.get("C:\\Users\\Administrador\\Desktop\\arquivo.txt");
-        List<String> linhas = Files.readAllLines(arquivo);
-        for (int i = 0; i < linhas.size(); i++) {
-            String nome = linhas.get(i);
-            System.out.println("Jogador " + i + ": " + nome);
+        List<String> linhas = Files.readAllLines(arquivo);  
+        for (int i = 0; i < linhas.size(); i++) { 
+             String nome = linhas.get(i);  
+            System.out.println("Jogador " + i + ": " + nome); 
+            
+            
+            
+   // O método size() da interface List em Java é usado para obter o número de elementos nesta lista. 
+        //Ou seja, esse método retorna a contagem de elementos presentes nesse contêiner de lista.
+        //Parâmetros: Este método não aceita nenhum parâmetro. Valor de retorno: Este método retorna o número de elementos nesta lista.
         }
     }
 }
