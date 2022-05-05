356d1be8d9b9f2a57cb656403876c3605bf56407
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-05 18:49:41 -0300

    Add files via upload

---
+++ orientacao_a_objetos/composicao_objetos/Carro.java
@@ -0,0 +1,14 @@
+package com.company.acessando_atributos_de_objetos.composicao_objetos;
+
+public class Carro {
+
+    String fabricante = "Sem fabricante";
+    String modelo;
+    String cor;
+    int anoDeFabricacao;
+
+
+
+
+    Proprietario dono;
+}
