a26aa53b2fb075930d582699304b947e39975f20
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-06 21:49:27 -0300

    Delete ExerciciosDeExemplo directory

--- ExerciciosDeExemplo/acessando_atributos_de_objetos/exercicio01/Cachorro.java
+++
@@ -1,9 +0,0 @@
-package com.company.algoitmos.avancados.acessando_atributos_de_objetos.exercicio01;
-
-public class Cachorro {
-
-    String nome;
-    String raca;
-    char sexo;
-    int idade;
-}
