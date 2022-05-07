b909795275f92a65adaac88c6bd5e3d129218f06
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-06 21:21:22 -0300

    Delete orientacao_a_objetos/composicao_objetos/exercicio02 directory

--- orientacao_a_objetos/composicao_objetos/exercicio02/Caminhada.java
+++
@@ -1,8 +0,0 @@
-package com.company.acessando_atributos_de_objetos.composicao_objetos.exercicio;
-
-public class Caminhada {
-
-    public void andar(Pessoa pessoa1) {
-        System.out.println("Eu \"" + pessoa1.nome + "\"" + "estou andando com o \"" + pessoa1.cachorro.nome);
-    }
-}
