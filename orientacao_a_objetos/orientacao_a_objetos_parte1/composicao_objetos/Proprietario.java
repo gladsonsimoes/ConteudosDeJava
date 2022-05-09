e2528e9516b7e01d7ae79ee856342b71bdf5cf60
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-09 19:26:47 -0300

    Add files via upload

---
+++ orientacao_a_objetos/orientacao_a_objetos_parte1/composicao_objetos/Proprietario.java
@@ -0,0 +1,12 @@
+package com.company.acessando_atributos_de_objetos.composicao_objetos;
+
+public class Proprietario {
+
+    String nome;
+    String cpf;
+    int idade;
+    String logradouro;
+    String bairro;
+    String cidade;
+
+}
