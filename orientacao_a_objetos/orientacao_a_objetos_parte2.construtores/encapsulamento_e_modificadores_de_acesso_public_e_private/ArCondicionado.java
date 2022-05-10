55a1afdea8a5b331e2254f5fd74dd6034bb2d4fd
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-09 21:18:41 -0300

    Add files via upload

--- orientacao_a_objetos/orientacao_a_objetos_parte2.construtores/encapsulamento_e_modificadores_de_acesso_public_e_private/ArCondicionado.java
+++ orientacao_a_objetos/orientacao_a_objetos_parte2.construtores/encapsulamento_e_modificadores_de_acesso_public_e_private/ArCondicionado.java
@@ -1,1 +1,12 @@
+package com.company.encapsulamento_e_modificadores_de_acesso_public_e_private;
 
+public class ArCondicionado {
+   private int temperatura;
+
+    public void trocarTemperatura(int temperatura) {
+        if (temperatura >= 17 && temperatura <= 25) {
+            this.temperatura = temperatura;
+            System.out.println("temperatura: " + temperatura);
+        }
+    }
+}
