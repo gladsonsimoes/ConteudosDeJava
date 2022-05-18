d77872b1b03351f6bb82a7e747488c7d127dbb7a
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-17 21:21:52 -0300

    Add files via upload

---
+++ orientacao_a_objetos/orientacao_a_objetos_parte2/modificador_de_acesso_default/Cachorro.java
@@ -0,0 +1,15 @@
+package com.company.modificador_de_acesso_default;
+
+public class Cachorro {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void sentar(){
+        System.out.println("\nEu, " + nome + ", vou sentar!");
+    }
+}
