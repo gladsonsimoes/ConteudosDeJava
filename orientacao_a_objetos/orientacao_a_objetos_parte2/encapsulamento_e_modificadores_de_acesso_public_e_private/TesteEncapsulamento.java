0ab7fcbee3be34a133a0933f7bcda692664ed21f
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-17 21:14:00 -0300

    Add files via upload

---
+++ orientacao_a_objetos/orientacao_a_objetos_parte2/encapsulamento_e_modificadores_de_acesso_public_e_private/TesteEncapsulamento.java
@@ -0,0 +1,8 @@
+package com.company.encapsulamento_e_modificadores_de_acesso_public_e_private;
+
+public class TesteEncapsulamento {
+    public static void main(String[] args) {
+        ArCondicionado ar = new ArCondicionado();
+        ar.trocarTemperatura(21);
+    }
+}
