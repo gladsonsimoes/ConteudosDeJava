efd3bfb95d85b21eaddef71e3a55cd0416039146
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-17 21:20:04 -0300

    Delete orientacao_a_objetos/orientacao_a_objetos_parte2.construtores directory

--- orientacao_a_objetos/orientacao_a_objetos_parte2.construtores/encapsulamento_e_modificadores_de_acesso_public_e_private/TesteEncapsulamento.java
+++
@@ -1,8 +0,0 @@
-package com.company.encapsulamento_e_modificadores_de_acesso_public_e_private;
-
-public class TesteEncapsulamento {
-    public static void main(String[] args) {
-        ArCondicionado ar = new ArCondicionado();
-        ar.trocarTemperatura(21);
-    }
-}
