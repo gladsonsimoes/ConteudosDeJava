4a4632c11013535e70d6c40694e4329ccd83d266
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-25 19:13:05 -0300

    Update Petshop.java

--- logica_de_programacao/acessando_atributos_de_objetos/exercicio01/Petshop.java
+++ logica_de_programacao/acessando_atributos_de_objetos/exercicio01/Petshop.java
@@ -1,6 +1,3 @@
-package com.company.algoitmos.avancados.acessando_atributos_de_objetos.exercicio01;
-
-
 public class Petshop {
     public static void main(String[] args) {
         Cachorro cachorro = new Cachorro();
