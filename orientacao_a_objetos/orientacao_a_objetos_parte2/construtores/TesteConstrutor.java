777a8a1133f0d14b00cedfdc7ac9437ab30a3623
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-25 21:05:47 -0300

    Update TesteConstrutor.java

--- orientacao_a_objetos/orientacao_a_objetos_parte2/construtores/TesteConstrutor.java
+++ orientacao_a_objetos/orientacao_a_objetos_parte2/construtores/TesteConstrutor.java
@@ -2,9 +2,9 @@ package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2.construtore
 
 public class TesteConstrutor {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa("João");
+        Pessoa p1 = new Pessoa("João"); //acessei a classe Pessoa e defini "João" para o construtor (String nome ) da classe pessoa.
         System.out.println("Nome: \"" + p1.nome + "\" tem " + p1.idade + " anos.");
-        Pessoa p2 = new Pessoa("Maria", 22);
+        Pessoa p2 = new Pessoa("Maria", 22); //acessei a classe Pessoa e defini "Maria" e 22 para o construtor (String nome,int idade) da classe Pessoa
         System.out.println("Nome: \"" + p2.nome + "\" tem " + p2.idade + " anos.");
 
 
