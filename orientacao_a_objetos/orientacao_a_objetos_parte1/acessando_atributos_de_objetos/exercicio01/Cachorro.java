7d6226fc5509e1172135880b551a1588e6770630
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-25 21:24:02 -0300

    Update Cachorro.java

--- orientacao_a_objetos/orientacao_a_objetos_parte1/acessando_atributos_de_objetos/exercicio01/Cachorro.java
+++ orientacao_a_objetos/orientacao_a_objetos_parte1/acessando_atributos_de_objetos/exercicio01/Cachorro.java
@@ -1,8 +1,8 @@
 package com.company.acessando_atributos_de_objetos.exercicio01;
 
 public class Cachorro {
-
-    String nome;
+  //criando objetos
+    String nome;  
     String raca;
     char sexo;
     int idade;
