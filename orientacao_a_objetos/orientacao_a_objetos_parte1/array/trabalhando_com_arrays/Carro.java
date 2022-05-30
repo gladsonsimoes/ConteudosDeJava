ff08bb49d06a4f61ec594908a72f1552c76651eb
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-05-30 13:27:51 -0300

    Create Carro.java

---
+++ orientacao_a_objetos/orientacao_a_objetos_parte1/array/trabalhando_com_arrays/Carro.java
@@ -0,0 +1,18 @@
+import com.company.orientacao_a_objetos.orientacao_a_objetos_parte1.composicao_objetos.Proprietario;
+
+public class Carro {
+
+    String fabricante;
+    String modelo;
+    String cor;
+    int anoDeFabricacao;
+    boolean biCombustivel;
+
+    Proprietario dono;
+
+    void ligar() {
+        if (modelo != null) {
+            System.out.println("Ligando o carro: " + modelo);
+        }
+    }
+}
