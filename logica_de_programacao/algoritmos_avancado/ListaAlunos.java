d61854ad103e64471b1ad6cf71ab610783c708f5
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-06-02 18:57:21 -0300

    Update ListaAlunos.java

--- logica_de_programacao/algoritmos_avancado/ListaAlunos.java
+++ logica_de_programacao/algoritmos_avancado/ListaAlunos.java
@@ -2,18 +2,18 @@ package com.company.algoritmos_avancados;
 
 public class ListaAlunos {
 	
-	static final int QUANTIDADE_LISTA = 5;
+	static final int QUANTIDADE_LISTA = 5; //constante 
 	
-	Aluno[] lista = new Aluno[QUANTIDADE_LISTA];
+	Aluno[] lista = new Aluno[QUANTIDADE_LISTA]; //vetor com a constante
 	
 	int tamanhoLista = 0;
 	
-	Aluno obter(int indice) {
-		return lista[indice];
+	Aluno obter(int indice) {  //invocando o array
+		return lista[indice];  //retornando o array de Aluno lista
 	}
 	
 	int tamanho() {
-		return tamanhoLista;
+		return tamanhoLista;  
 	}
 	
 	void adicionar(Aluno aluno) {
@@ -27,7 +27,7 @@ public class ListaAlunos {
 			lista = novaLista;
 		}
 		
-		lista[tamanhoLista] = aluno;
+		lista[tamanhoLista] = aluno; 
 		tamanhoLista++;
 	}
 }
