2045a8f2b0a67baf526f606df18554ceb9983245
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-10-25 14:46:47 -0300

    Update OperadoresAtribuicao.java

--- src/com/company/logica_de_programacao/operadores/OperadoresAtribuicao.java
+++ src/com/company/logica_de_programacao/operadores/OperadoresAtribuicao.java
@@ -4,21 +4,27 @@ public class OperadoresAtribuicao {
     public static void main(String[] args) {
         Double numero = 7.0;
 
-		numero = numero + 4;
-		System.out.println("numero + 4: " + numero);
+        //somar numero + 4
+        numero = numero + 4;
+        System.out.println("numero + 4: " + numero);
 
-		numero += 4;
-		System.out.println("numero += 4: " + numero);
+        //pegar o resultado do numero + 4 , e adicionar mais 4
+        numero += 4;
+        System.out.println("numero += 4: " + numero);
 
-		numero -= 4;
-		System.out.println("numero -= 4: " + numero);
+        //pegar o ultimo resultado e diminuir
+        numero -= 4;
+        System.out.println("numero -= 4: " + numero);
 
-		numero *= 4;
-		System.out.println("numero *= 4: " + numero);
+        //pegar o ultimo resultado e multiplicar
+        numero *= 4;
+        System.out.println("numero *= 4: " + numero);
 
-		numero /= 4;
-		System.out.println("numero /= 4: " + numero);
+        //pegar e dividir o ultimo resultado
+        numero /= 4;
+        System.out.println("numero /= 4: " + numero);
 
+        //para saber o resto da divisão!
         numero %= 4;
         System.out.println("numero %= 4: " + numero);
     }
