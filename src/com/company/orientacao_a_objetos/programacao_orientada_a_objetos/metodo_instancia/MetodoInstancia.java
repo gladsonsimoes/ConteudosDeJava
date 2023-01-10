7132588ab816e3487ec6c152a1ff426e1ff4afac
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-01-10 09:03:59 -0300

    Update MetodoInstancia.java

--- src/com/company/orientacao_a_objetos/programacao_orientada_a_objetos/metodo_instancia/MetodoInstancia.java
+++ src/com/company/orientacao_a_objetos/programacao_orientada_a_objetos/metodo_instancia/MetodoInstancia.java
@@ -3,20 +3,21 @@ package com.company.orientacao_a_objetos.programacao_orientada_a_objetos.metodo_
 public class MetodoInstancia {
 
     public static void main(String[] args) {
-        Cliente cliente = new Cliente();
-        cliente.primeiroNome = "Alexandre";
+        Cliente cliente = new Cliente(); //Objeto cliente da Classe cliente
+        cliente.primeiroNome = "Alexandre"; 
         cliente.ultimoNome = "Afonso";
         cliente.telefone = "349000000";
         cliente.email = "alexandre.afonso@algawork.com";
 
-        Cliente cliente2 = new Cliente();
+        Cliente cliente2 = new Cliente(); //Objeto cliente2 da classe Cliente
         cliente2.primeiroNome = "João";
         cliente2.ultimoNome = "Silva";
         cliente2.telefone = "3489999999";
 
+         //metodo estatico para todos os clientes
+//		 System.out.println("Nome cliente: " + Cliente.obterNomeCompleto(cliente));
 
-//		 System.out.println("Nome cliente: " + Cliente.obterNomeCompleto(cliente)); // Esse é o método estático.
-
+        //metodo para cada liente
         System.out.println("Nome cliente: " + cliente.obterNomeCompleto() + ", DDD: " + cliente.obterDDD());
         System.out.println("Nome cliente: " + cliente2.obterNomeCompleto() + ", DDD: " + cliente2.obterDDD());
     }
