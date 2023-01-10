08dd33b7a9c76295cb7256b579993591d80660ab
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-01-10 08:35:36 -0300

    Update Cliente.java

--- src/com/company/orientacao_a_objetos/programacao_orientada_a_objetos/metodo_instancia/Cliente.java
+++ src/com/company/orientacao_a_objetos/programacao_orientada_a_objetos/metodo_instancia/Cliente.java
@@ -1,15 +1,19 @@
 package com.company.orientacao_a_objetos.programacao_orientada_a_objetos.metodo_instancia;
 
 public class Cliente {
+    //atributos da classe Cliente
     String primeiroNome;
     String ultimoNome;
     String telefone;
     String email;
+    
+    //metodo para agrupar o nome completo
     String obterNomeCompleto() {
         String nomeCompleto = primeiroNome + " " + ultimoNome;
         return nomeCompleto;
     }
 
+    //metodo para pegar o ddd do telefone
     String obterDDD() {
         String ddd = telefone.substring(0, 2);
         return ddd;
