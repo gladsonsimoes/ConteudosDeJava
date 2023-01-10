16d91cebfc00d77e29d890e1c8d55124b3405762
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-01-10 08:22:10 -0300

    Update Carro.java

--- src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/o_objeto_this/Carro.java
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte2/o_objeto_this/Carro.java
@@ -3,15 +3,17 @@ package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2.o_objeto_th
 import com.company.orientacao_a_objetos.orientacao_a_objetos_parte1.composicao_objetos.Proprietario;
 
 public class Carro {
+    //atributos da Classe carro
     String fabricante;
     String modelo;
     String  cor;
     int anoDeFabricacao;
     boolean biCombustivel;
     Proprietario dono;
+    
     void alterarModelo(String modelo){
         if (modelo != null) {
-            this.modelo = modelo;
+            this.modelo = modelo; 
         }
     }
 
